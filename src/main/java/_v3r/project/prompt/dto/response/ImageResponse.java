1bc3cd6e48f9781d72e0dd00cfbe2cfab905051e
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-05-09 20:42:17 +0900

    [refactor] 이미지 생성시 promptId추가 #17

--- src/main/java/_v3r/project/prompt/dto/response/ImageResponse.java
+++ src/main/java/_v3r/project/prompt/dto/response/ImageResponse.java
@@ -3,7 +3,7 @@ package _v3r.project.prompt.dto.response;
 import _v3r.project.prompt.domain.enumtype.Paints;
 import java.util.List;
 
-public record ImageResponse(Paints paints, List<ImageData> data) {
+public record ImageResponse(Long promptId,Paints paints, List<ImageData> data) {
     public record ImageData(String url) {}
 
 }
