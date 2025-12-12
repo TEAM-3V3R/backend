4722153b2937c560bdf032441384d024f7ea4398
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-12-13 03:15:29 +0900

    [remove] 인페인팅 기능 삭제 #118

--- src/main/java/_v3r/project/prompt/controller/PromptController.java
+++ src/main/java/_v3r/project/prompt/controller/PromptController.java
@@ -3,12 +3,10 @@ package _v3r.project.prompt.controller;
 import _v3r.project.common.apiResponse.CustomApiResponse;
 import _v3r.project.prompt.domain.enumtype.Paints;
 import _v3r.project.prompt.dto.request.ChatRequest;
-import _v3r.project.prompt.dto.request.InpaintingImageRequest;
 import _v3r.project.prompt.dto.response.ImageResponse;
 import _v3r.project.prompt.service.PromptService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
