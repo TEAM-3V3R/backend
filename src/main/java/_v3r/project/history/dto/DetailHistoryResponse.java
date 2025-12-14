11257d279b0cdbd83cdbf2c717002171b7d1fa77
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-12-14 18:29:01 +0900

    [refactor] 히스토리 관련 response 빌더패턴으로 수정 #118

--- src/main/java/_v3r/project/history/dto/DetailHistoryResponse.java
+++ src/main/java/_v3r/project/history/dto/DetailHistoryResponse.java
@@ -1,19 +1,53 @@
 package _v3r.project.history.dto;
 
+import _v3r.project.prompt.domain.Chat;
+import _v3r.project.prompt.domain.Prompt;
 import _v3r.project.prompt.domain.enumtype.Paints;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record DetailHistoryResponse(
         Long chatId,
+        String chatTitle,
         Paints paints,
         List<PromptHistory> prompts,
-        String finalImageUrl // lastPrompt의 image_url
+        String finalImageUrl  // 채팅방의 마지막 사진 url
 ) {
+
     public record PromptHistory(
             LocalDateTime createdAt,
             String promptContent,
             String imageUrl,
             List<String> classifications
-    ) {}
+    ) {
+        public static PromptHistory of(
+                Prompt prompt,
+                List<String> classifications
+        ) {
+            return new PromptHistory(
+                    prompt.getCreatedAt(),
+                    prompt.getPromptContent(),
+                    prompt.getImageUrl(),
+                    classifications
+            );
+        }
+    }
+
+    public static DetailHistoryResponse of(
+            Chat chat,
+            List<PromptHistory> promptHistories
+    ) {
+        PromptHistory lastPrompt =
+                promptHistories.get(promptHistories.size() - 1);
+
+        return DetailHistoryResponse.builder()
+                .chatId(chat.getChatId())
+                .chatTitle(chat.getChatTitle())
+                .paints(chat.getPaints())
+                .prompts(promptHistories)
+                .finalImageUrl(lastPrompt.imageUrl())
+                .build();
+    }
 }
