c087169bc01e22807769711b9ed989e784cfb80a
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-12-23 18:37:43 +0900

    [refactor] paints 책임 분리 및 스타일링프롬프트 enum 클래스 분리 #153

--- src/main/java/_v3r/project/prompt/domain/enumtype/Paints.java
+++ src/main/java/_v3r/project/prompt/domain/enumtype/Paints.java
@@ -1,6 +1,15 @@
 package _v3r.project.prompt.domain.enumtype;
 
 public enum Paints {
-    // 어해도 : 0 산수도 : 1 탱화 : 2
-    어해도,산수도,탱화
+    어해도(PromptTemplate.어해도),
+    산수도(PromptTemplate.산수도),
+    탱화(PromptTemplate.탱화);
+    private final PromptTemplate template;
+    Paints(PromptTemplate template) {
+        this.template = template;
+    }
+
+    public PromptTemplate getTemplate() {
+        return template;
+    }
 }
